8c9af60d76240ecdba7ef402f503ebf2baf05d7b
Kirtan Pathak <kirtanpathak4@gmail.com>
2022-01-15 10:54:25 -0600

    get and delete route request commit

--- Node/routes/route.js
+++ Node/routes/route.js
@@ -3,6 +3,18 @@ const Model = require("../models/model");
 
 const router = express.Router();
 
+//GET - Request for all TODO's
+router.get("/", async (req, res) => {
+    try {
+        const getAllTodo = await Model.find();
+        res.send(getAllTodo);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+
+//POST - Request to add new TODO
 router.post("/", async (req, res) => {
     try {
         const newTodo = await new Model(req.body).save();
@@ -11,5 +23,16 @@ router.post("/", async (req, res) => {
         res.send(error);
     }
 });
+
+//DELETE - Request to delete a TODO
+router.delete("/:id", async (req, res) => {
+    try {
+        const deleteTodo = await Model.findByIdAndDelete(req.params.id);
+        res.send(deleteTodo);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
 
 module.exports = router;
